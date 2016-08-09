3cd8e8e7c1b2c48b7995b7d797fccdeefaab6a8a
exceptionnotfound <mpjones428@gmail.com>
2016-08-09 08:31:44 -0700

    Add project files.

---
+++ src/ViewInjectionCoreDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewInjectionCoreDemo.Controllers
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("all")]
+        [Route("~/")]
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ViewInjectionCoreDemo/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewInjectionCoreDemo.Repositories
+{
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
+public interface IUserRepository
+{
+    List<User> GetAll();
+}
+
+public class UserRepository : IUserRepository
+{
+    public List<User> GetAll()
+    {
+        return new List<User>()
+        {
+            new User()
+            {
+                FirstName = "Jyn",
+                LastName = "Erso",
+                DateOfBirth = new DateTime(1983, 10, 17)
+            },
+            new User()
+            {
+                FirstName = "Cassion",
+                LastName = "Andor",
+                DateOfBirth = new DateTime(1979, 12, 29)
+            },
+            new User()
+            {
+                FirstName = "Bodhi",
+                LastName = "Rook",
+                DateOfBirth = new DateTime(1982, 12, 1)
+            }
+        };
+    }
+}
+}
